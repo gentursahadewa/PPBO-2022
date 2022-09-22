73d76f7dcce43b0e656f3809a95850aa08a58095
Gentur Sahadewa <91191446+gentursahadewa@users.noreply.github.com>
2022-09-22 16:44:10 +0700

    Add files via upload
    while loop

---
+++ Praktikum 3 - Pak Ardhi (bagian 2)/WhileLoopDemo.java
@@ -0,0 +1,16 @@
+//M0521023_Gentur Sahadewa
+import java.util.concurrent.TimeUnit;
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int i = 3;
+        while (i <=300) {
+            System.out.println("Hello ke-" + i);
+            i = i + 3;
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (Exception x) {
+                x.printStackTrace();
+            }
+        }
+    }
+}

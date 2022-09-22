2b8226bcd14ae6fba273c549eac7e21cc3665bc8
Gentur Sahadewa <91191446+gentursahadewa@users.noreply.github.com>
2022-09-22 16:37:24 +0700

    Create WhileLoopDemo.java
    do while loop

---
+++ Praktikum 3 - Pak Ardhi (bagian 2)/WhileLoopDemo.java
@@ -0,0 +1,16 @@
+//M0521023_Gentur Sahadewa
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 4, 2, 3};
+        /* PERULANGAN FOR-LOOP
+        for (int i = 0; i < 5; i++) {
+            System.out.println("elemen ke-" + (i+1) + " = " + arr[i]);
+        }*/
+        // PERULANGAN DO WHILE
+        int i = 0;
+        do {
+            System.out.println("elemen ke-" + (i+1) + " = " + arr[i]);
+            i++; 
+        } while(i < 5);
+    }
+}
